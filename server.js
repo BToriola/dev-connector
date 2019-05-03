7a9c0300ebc70ea67f3e92060bcd0dfb613943f6
RELE <apple@APPLEs-MacBook-Pro.local>
2019-05-03 08:14:59 +0100

    User registration endpoint done

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require("./config/keys").mongoURI;
